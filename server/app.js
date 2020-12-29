093efc72e8f16045f72abdfd5954631c5623c79f
soaelee <soae0923@gmail.com>
2020-12-29 16:12:39 +0900

    mysql, sequelize 적용

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const postRouter = require('./routes/post');
 
 const app = express();
 
@@ -18,17 +19,7 @@ app.get('/api/posts', (req, res) => {
   ])
 })
 
-app.post('/api/post', (req, res) => {
-  res.json({
-    id: 1, content: 'hello'
-  })
-})
-
-app.delete('/api/post', (req, res) => {
-  res.json({
-    id: 1
-  })
-})
+app.use('/post', postRouter);
 
 app.listen(3065, () => {
   console.log('🐶server🚀');
